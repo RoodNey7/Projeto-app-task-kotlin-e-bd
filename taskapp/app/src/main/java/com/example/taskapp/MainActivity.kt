07336c1667ccc435eda405cf1959e79243a14e94
Guilherme Vinhas <100172462+RoodNey7@users.noreply.github.com>
2023-10-23 21:53:43 -0300

    Mudanças na tela de login
    Melhorias na tela de login, agora é possível fazer o acesso ao github dos criadores por meio do próprio app

--- taskapp/app/src/main/java/com/example/taskapp/MainActivity.kt
+++ taskapp/app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.taskapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.taskapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+lateinit var binding: ActivityMainBinding
+public class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root);
+
+
+        val abrir: TextView=findViewById(R.id.textView8)
+
+
+        abrir.setOnClickListener{
+            val github= Intent(Intent.ACTION_VIEW)
+            github.data= Uri.parse("https://github.com/Rogerms22")
+
+            startActivity(github)
+
+                val gui: TextView=findViewById(R.id.textView15)
+
+                gui.setOnClickListener{
+                    val opengit= Intent(Intent.ACTION_VIEW)
+                    opengit.data= Uri.parse("https://github.com/RoodNey7")
+
+                    startActivity(opengit)
+        }
+    } } }
